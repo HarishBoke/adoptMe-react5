f3556c95fbc6ad860d4aea498a2295611da7e3d5
Harish Boke <harish.boke@gmail.com>
2021-01-28 14:51:13 +0530

    Setup: Parcel, React , React-dom

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
+import React from 'react'
+import { render } from 'react-dom'
 
-const Pet = ({name, animal, breed}) => {
-    return React.createElement('div', {},
-        [
-            React.createElement('h1', {}, name),
-            React.createElement('h2', {}, animal),
-            React.createElement('h3', {}, breed)
-            // element , props like id , child
-        ]
-    )
-}
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h3", {}, breed),
+    // element , props like id , child
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        'div',
-        { id: 'something-important'},
-            React.createElement('h1', {}, 'Adopt Me!'),
-           // React.createElement(Pet), // other two params optional
-            React.createElement(Pet, {
-                name: 'Sheru',
-                animal: 'Dog',
-                breed: 'Havanese',
-            }),
-            React.createElement(Pet, {
-                name: 'Pepper',
-                animal: 'Bird',
-                breed: 'Cockatiel',
-            }),
-            React.createElement(Pet, {
-                name: 'Sizzer',
-                animal: 'Cat',
-                breed: 'KNQ',
-            }),
-    )
-}
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+  return React.createElement(
+    "div",
+    { id: "something-important" },
+    React.createElement("h1", {}, "Adopt Me!"),
+    
+    // React.createElement(Pet), // other two params optional
+    React.createElement(Pet, {
+      name: "Sheru",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sizzer",
+      animal: "Cat",
+      breed: "KNQ",
+    })
+  );
+};
+render(React.createElement(App), document.getElementById("root"));
