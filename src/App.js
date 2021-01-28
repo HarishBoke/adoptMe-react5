43588c1e6c2f42fa11bd6a389efff1d9c25917cd
Harish Boke <harish.boke@gmail.com>
2021-01-28 12:16:15 +0530

    Using Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement('div', {},
         [
-            React.createElement('h1', {}, 'Sheru'),
-            React.createElement('h2', {}, 'Dog'),
-            React.createElement('h3', {}, 'Havanese')
+            React.createElement('h1', {}, props.name),
+            React.createElement('h2', {}, props.animal),
+            React.createElement('h3', {}, props.breed)
             // element , props like id , child
         ]
     )
@@ -14,13 +14,23 @@ const App = () => {
     return React.createElement(
         'div',
         { id: 'something-important'},
-        [
             React.createElement('h1', {}, 'Adopt Me!'),
-            React.createElement(Pet), // other two params optional
-            React.createElement(Pet),
-            React.createElement(Pet),
-
-        ]
+           // React.createElement(Pet), // other two params optional
+            React.createElement(Pet, {
+                name: 'Sheru',
+                animal: 'Dog',
+                breed: 'Havanese',
+            }),
+            React.createElement(Pet, {
+                name: 'Pepper',
+                animal: 'Bird',
+                breed: 'Cockatiel',
+            }),
+            React.createElement(Pet, {
+                name: 'Sizzer',
+                animal: 'Cat',
+                breed: 'KNQ',
+            }),
     )
 }
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
