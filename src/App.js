1e432bf2e87775af2c98a144e9534c5e57cf2f62
Harish Boke <harish.boke@gmail.com>
2021-01-28 14:55:16 +0530

    separate components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h3", {}, breed),
-    // element , props like id , child
-  ]);
-};
+import Pet from './Pet';
 
 const App = () => {
   return React.createElement(
